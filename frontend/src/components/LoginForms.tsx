84d2bbe1b9beaf580cabc479753f77df0ab54dfd
xxMichalPK <michal10.05.pk@gmail.com>
2023-03-07 19:32:17 +0100

    Add simple registration and login validation

--- frontend/src/components/LoginForms.tsx
+++ frontend/src/components/LoginForms.tsx
@@ -7,7 +7,10 @@ import {
     Title,
     Text,
     rem,
+    Button,
+    useMantineTheme,
   } from '@mantine/core';
+import { useForm } from '@mantine/form';
   import { Link } from 'react-router-dom';
   
   const useStyles = createStyles((theme) => ({
@@ -37,63 +40,84 @@ import {
     },
 
     button: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        lineHeight: 1,
-        margin: '15px 0',
-        padding: '15px 25px',
-        borderRadius: theme.radius.sm,
-        textDecoration: 'none',
-        fontSize: theme.fontSizes.md,
-        fontWeight: 500,
-    
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+      lineHeight: 1.4,
+      margin: '15px 0',
+      padding: '15px 25px',
+      borderRadius: theme.radius.sm,
+      textDecoration: 'none',
+      fontSize: theme.fontSizes.md,
+      fontWeight: 500,
+  
+      color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
+      // backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.light[4],
+  
+      // '&:hover': {
+      //   backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.light[6],
+      // },
+    },
+
+    textInput: {
+      '&>div>input': {
+        backgroundColor: 'transparent',
         color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.light[4],
-    
-        '&:hover': {
-          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.light[6],
-        },
-      },
-
-      textInput: {
-        '&>div>input': {
-          backgroundColor: 'transparent',
-          color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
-        }
-      },
-
-      passwdInput: {
-        '&>div>div, &>div>div>input': {
-          backgroundColor: 'transparent',
-          color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
-        },
-        '&>div>div:nth-of-type(2)>button:hover': {
-          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.light[4],
-        }
-      },
-
-      checkBox: {
-        '&>div>div>input': {
-          backgroundColor: 'transparent',
-        }
       }
+    },
+
+    passwdInput: {
+      '&>div>div, &>div>div>input': {
+        backgroundColor: 'transparent',
+        color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
+      },
+      '&>div>div:nth-of-type(2)>button:hover': {
+        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.light[4],
+      }
+    },
+
+    checkBox: {
+      '&>div>div>input': {
+        backgroundColor: 'transparent',
+      }
+    }
   }));
   
   function SignInForm() {
     const { classes } = useStyles();
+    const mainTheme = useMantineTheme();
+
+    const loginValidation = useForm({
+      initialValues: {
+        email: '',
+      },
+
+      validateInputOnBlur: true,
+
+      validate: {
+        email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email address')
+      },
+    });
+
     return (
       <div className={classes.wrapper}>
         <Paper className={classes.form} radius={0} p={30}>
           <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
             Welcome back to Dcorpo!
           </Title>
-          <TextInput className={classes.textInput} label="Email address" placeholder="hello@dcorpo.org" size="md" />
-          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Your password" mt="md" size="md" />
-          <Checkbox className={classes.checkBox} label="Keep me logged in" mt="xl" size="md" />
-          <Link to="/auth_login" className={classes.button}>
-            Login
-          </Link>
+          <form onSubmit={loginValidation.onSubmit((values) => console.log(values))}>
+            <TextInput className={classes.textInput} label="Email address" placeholder="example@dcorpo.org" size="md" {...loginValidation.getInputProps('email')} />
+            <PasswordInput className={classes.passwdInput} label="Password" placeholder="Password" mt="md" size="md" />
+            <Checkbox className={classes.checkBox} label="Keep me logged in" mt="xl" size="md" />
+            <Button type='submit' className={classes.button} color={mainTheme.colorScheme === 'dark' ? 'dark.3' : 'light.4'}>
+              Login
+            </Button>
+            {/* <Link to="/auth_login" className={classes.button}>
+              Login
+            </Link> */}
+          </form>
           <Text ta="center" mt="md">
             Don&apos;t have an account?{' '}
             <Link to="/sign_up">
@@ -107,20 +131,51 @@ import {
 
   function SignUpForm() {
     const { classes } = useStyles();
+    const mainTheme = useMantineTheme();
+
+    const registerValidation = useForm({
+      initialValues: {
+        email: '',
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        termsOfService: false,
+      },
+
+      validateInputOnBlur: true,
+
+      validate: {
+        email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email address'),
+        username: (value) => (value.length < 4 ? 'The username has to be at least 4 characters long' : null),
+        password: (value) => (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[,.@$!%*?&])[A-Za-z\d,.@$!%*?&]{8,}$/.test(value) ? null : 'The password must contain minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character'),
+        passwordConfirm: (value, values) => (
+          value !== values.password ? 'Passwords do not match' : null
+        ),
+        termsOfService: (value) => (value === false ? 'You have to agree to terms of service' : null)
+      },
+    });
+
     return (
       <div className={classes.wrapper}>
         <Paper className={classes.form} radius={0} p={30}>
           <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
             Welcome to Dcorpo!
           </Title>
-          <TextInput className={classes.textInput} label="Email address" placeholder="hello@dcorpo.org" size="md" />
-          <TextInput className={classes.textInput} label="Username" placeholder="My_Username" mt="md" size="md" />
-          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Set password" mt="md" size="md" />
-          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Repeat password" mt="md" size="md" />
-          <Checkbox className={classes.checkBox} label="I read and agree to terms" mt="xl" size="md" />
-          <Link to="/auth/registration/" className={classes.button}>
-            Register
-          </Link>
+          <form onSubmit={registerValidation.onSubmit((values) => console.log(values))}>
+            <TextInput className={classes.textInput} label="Email address" placeholder="example@dcorpo.org" size="md" {...registerValidation.getInputProps('email')} />
+            <TextInput className={classes.textInput} label="Username" placeholder="Username" mt="md" size="md" {...registerValidation.getInputProps('username')} />
+            <PasswordInput className={classes.passwdInput} label="Password" placeholder="Password" mt="md" size="md" {...registerValidation.getInputProps('password')} />
+            <PasswordInput className={classes.passwdInput} label="Password" placeholder="Repeat password" mt="md" size="md" {...registerValidation.getInputProps('passwordConfirm')} />
+            <Checkbox className={classes.checkBox} label="I read and agree to terms" mt="xl" size="md" {...registerValidation.getInputProps('termsOfService', { type: 'checkbox' })} />
+            {/* <Link to="/auth/registration/" className={classes.button}>
+              Register
+            </Link> */}
+            <Button type='submit' className={classes.button} color={mainTheme.colorScheme === 'dark' ? 'dark.3' : 'light.4'}>
+              Register
+            </Button>
+          </form>
+
           <Text ta="center" mt="md">
             Already have an account?{' '}
             <Link to="/sign_in">
--- frontend/src/theme/AppTheme.tsx
+++ frontend/src/theme/AppTheme.tsx
@@ -3,7 +3,7 @@ import { MantineThemeOverride } from '@mantine/core';
 export const AppTheme: MantineThemeOverride = {
     colorScheme: 'light',
     colors: {
-        dark: ['#5C5F66', '#373A40', '#333333', '#2B2A29', '#25262B', '#1A1B1E', '#181819', '#141517', '#101113', '#000000'],
+        dark: ['#5C5F66', '#373A40', '#333333', '#292929', '#25262B', '#1A1B1E', '#181819', '#141517', '#101113', '#000000'],
         gray: ['#CED4DA', '#ADB5BD', '#898989', '#495057', '#343A40'],
         light: ['#FFFFFF', '#F8F9FA', '#F1F3F5', '#EFEFEF', '#E9ECEF', '#DEE2E6', '#DADADA', '#D1D3D5', '#CED4DA', '#BABABA']
     }
