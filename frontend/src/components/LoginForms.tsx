27a663d083d30a8fde2f3daf07b176f3249808f5
xxMichalPK <michal10.05.pk@gmail.com>
2023-03-07 18:19:50 +0100

    Improve Login System colors + fix scrolling issue

--- frontend/src/components/LoginForms.tsx
+++ frontend/src/components/LoginForms.tsx
@@ -12,20 +12,23 @@ import {
   
   const useStyles = createStyles((theme) => ({
     wrapper: {
-      minHeight: rem(900),
+      height: 'calc(100vh - 56px)',
     },
   
     form: {
       borderRight: `${rem(1)} solid ${
         theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.gray[1]
       }`,
-      minHeight: rem(900),
+      height: '100%',
       maxWidth: rem(450),
       paddingTop: rem(80),
   
       [theme.fn.smallerThan('sm')]: {
         maxWidth: '100%',
       },
+
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.light[0],
+      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.light[6]
     },
   
     title: {
@@ -52,6 +55,29 @@ import {
           backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[2] : theme.colors.light[6],
         },
       },
+
+      textInput: {
+        '&>div>input': {
+          backgroundColor: 'transparent',
+          color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
+        }
+      },
+
+      passwdInput: {
+        '&>div>div, &>div>div>input': {
+          backgroundColor: 'transparent',
+          color: theme.colorScheme === 'dark' ? theme.colors.gray[1] : theme.colors.gray[4],
+        },
+        '&>div>div:nth-of-type(2)>button:hover': {
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.light[4],
+        }
+      },
+
+      checkBox: {
+        '&>div>div>input': {
+          backgroundColor: 'transparent',
+        }
+      }
   }));
   
   function SignInForm() {
@@ -62,9 +88,9 @@ import {
           <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
             Welcome back to Dcorpo!
           </Title>
-          <TextInput label="Email address" placeholder="hello@dcorpo.org" size="md" />
-          <PasswordInput label="Password" placeholder="Your password" mt="md" size="md" />
-          <Checkbox label="Keep me logged in" mt="xl" size="md" />
+          <TextInput className={classes.textInput} label="Email address" placeholder="hello@dcorpo.org" size="md" />
+          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Your password" mt="md" size="md" />
+          <Checkbox className={classes.checkBox} label="Keep me logged in" mt="xl" size="md" />
           <Link to="/auth_login" className={classes.button}>
             Login
           </Link>
@@ -87,11 +113,11 @@ import {
           <Title order={2} className={classes.title} ta="center" mt="md" mb={50}>
             Welcome to Dcorpo!
           </Title>
-          <TextInput label="Email address" placeholder="hello@dcorpo.org" size="md" />
-          <TextInput label="Username" placeholder="My_Username" size="md" />
-          <PasswordInput label="Password" placeholder="Set password" mt="md" size="md" />
-          <PasswordInput label="Password" placeholder="Repeat password" mt="md" size="md" />
-          <Checkbox label="I read and agree to terms" mt="xl" size="md" />
+          <TextInput className={classes.textInput} label="Email address" placeholder="hello@dcorpo.org" size="md" />
+          <TextInput className={classes.textInput} label="Username" placeholder="My_Username" mt="md" size="md" />
+          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Set password" mt="md" size="md" />
+          <PasswordInput className={classes.passwdInput} label="Password" placeholder="Repeat password" mt="md" size="md" />
+          <Checkbox className={classes.checkBox} label="I read and agree to terms" mt="xl" size="md" />
           <Link to="/auth/registration/" className={classes.button}>
             Register
           </Link>
